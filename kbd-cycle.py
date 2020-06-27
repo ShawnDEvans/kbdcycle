8c65aa06ffe57b4b0a134171eff1db6232ff9da9
ShawnDEvans <ShawnDEvans@gmail.com>
2020-06-27 13:03:17 -0400

    Added a lookup table to reduce sin function calls

--- kbd-cycle.py
+++ kbd-cycle.py
@@ -5,16 +5,16 @@ import argparse
 import sys
 import time
 
-def mix(frequency, phase, middle, width, length, pane_phase):
-    red =  math.sin(frequency * length + 0 + pane_phase) * width + middle 
-    green =  math.sin(frequency * length + (phase) + pane_phase) * width + middle 
-    blue =  math.sin(frequency * length + (phase*2) + pane_phase) * width + middle
+def mix(frequency, phase, middle, width, length):
+    red =  math.sin(frequency * length + 0) * width + middle 
+    green =  math.sin(frequency * length + (phase)) * width + middle 
+    blue =  math.sin(frequency * length + (phase*2)) * width + middle
     return f'{int(red):02x}{int(green):02x}{int(blue):02x}' 
 
 if __name__ == '__main__':
 
     example = 'Examples:\n\n'
-    example += '$ .{}'.format(sys.argv[0]) 
+    example += '$ .{} -f .3 --delay .25'.format(sys.argv[0]) 
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, epilog=example)
 
@@ -27,21 +27,28 @@ if __name__ == '__main__':
     amplitude = 127
     middle = 128
     counter = 0
+    
+    phase_array = []
 
+    for rgb in range(0,254):
+        left_mix = mix(frequency, 2, middle, amplitude, rgb) 
+        center_mix = mix(frequency, 2.5, middle, amplitude, rgb)
+        right_mix = mix(frequency, 3, middle, amplitude, rgb)
+        phase_array.append([left_mix, center_mix, right_mix])
+        
     while True:
-        left_mix = mix(frequency, 2, middle, amplitude, counter, 0) 
-        center_mix = mix(frequency, 2, middle, amplitude, counter, .5)
-        right_mix = mix(frequency, 2, middle, amplitude, counter, 1)
-        left = open('/sys/devices/platform/system76/leds/system76::kbd_backlight/color_left', 'w')
-        left.write(left_mix)
-        left.close()
-        center =  open('/sys/devices/platform/system76/leds/system76::kbd_backlight/color_center', 'w')
-        center.write(center_mix)
-        center.close()
-        right = open('/sys/devices/platform/system76/leds/system76::kbd_backlight/color_right', 'w')
-        right.write(right_mix)
-        right.close()
-        counter+=1
-        time.sleep(float(args.delay))
-        if counter > 255:
-            counter = 0
+        for phase in phase_array:
+            left_pane = phase[0]
+            center_pane = phase[1]
+            right_pane = phase[2]
+            left = open('/sys/devices/platform/system76/leds/system76::kbd_backlight/color_left', 'w')
+            left.write(left_pane)
+            left.close()
+            center =  open('/sys/devices/platform/system76/leds/system76::kbd_backlight/color_center', 'w')
+            center.write(center_pane)
+            center.close()
+            right = open('/sys/devices/platform/system76/leds/system76::kbd_backlight/color_right', 'w')
+            right.write(right_pane)
+            right.close()
+            counter+=1
+            time.sleep(float(args.delay))
